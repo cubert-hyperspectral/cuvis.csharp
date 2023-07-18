6a0dc8acff9a0a94f37fdb72742fa2e3aa82a73b
Arnd <brandes@cubert-gmbh.de>
2023-07-18 15:28:57 +0200

    updating intermediate files location

--- cuvis_netstubs.cmake
+++ cuvis_netstubs.cmake
@@ -4,64 +4,64 @@ cmake_minimum_required(VERSION 3.10)
 set(target_name cuvis_netstubs)
 
 set(PROJECT_FILES
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_cuvis_data_type_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_cuvis_operation_mode_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_cuvis_hardware_state_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_double.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_p_q_const__char_int__void.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_p_q_const__wchar_t_int__void.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_cuvis_data_type_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_cuvis_operation_mode_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_cuvis_hardware_state_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_double.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_p_q_const__char_int__void.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_p_q_const__wchar_t_int__void.cs
 
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_int_int__void.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_int_int__void.cs
 
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_acquisition_event_t__void.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_base_event_t__void.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_component_event_t__void.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_processing_event_t__void.cs
-# ${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_quality_event_t__void.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_acquisition_event_t__void.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_base_event_t__void.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_component_event_t__void.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_processing_event_t__void.cs
+# ${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_f_q_const__cuvis_quality_event_t__void.cs
 
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_int.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_unsigned_char.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_unsigned_int.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/SWIGTYPE_p_void.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_il.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_ilPINVOKE.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_component_info_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_component_type_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_data_type_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_export_general_settings_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_export_tiff_settings_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_export_view_settings_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_gps_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_hardware_state_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_imbuffer_format_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_imbuffer_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_loglevel_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_mesu_metadata_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_operation_mode_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_pan_sharpening_algorithm_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_pan_sharpening_interpolation_type_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_proc_args_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_processing_mode_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_reference_type_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_save_args_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_session_info_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_sensor_info_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_status_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_tiff_compression_mode_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_tiff_format_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_view_category_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_view_data_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_viewer_settings_t.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/LogHandlerBase.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/LocalizedLogHandlerBase.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_event_acquisition_data_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_event_base_data_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_event_component_data_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_event_processing_event_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_event_qualitiy_event_t.cs
-#${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/EventCallback.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_worker_settings_t.cs
-${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csil/cuvis_session_item_type_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_int.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_unsigned_char.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_unsigned_int.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/SWIGTYPE_p_void.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_il.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_ilPINVOKE.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_component_info_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_component_type_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_data_type_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_export_general_settings_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_export_tiff_settings_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_export_view_settings_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_gps_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_hardware_state_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_imbuffer_format_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_imbuffer_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_loglevel_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_mesu_metadata_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_operation_mode_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_pan_sharpening_algorithm_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_pan_sharpening_interpolation_type_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_proc_args_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_processing_mode_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_reference_type_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_save_args_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_session_info_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_sensor_info_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_status_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_tiff_compression_mode_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_tiff_format_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_view_category_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_view_data_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_viewer_settings_t.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/LogHandlerBase.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/LocalizedLogHandlerBase.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_event_acquisition_data_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_event_base_data_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_event_component_data_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_event_processing_event_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_event_qualitiy_event_t.cs
+#${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/EventCallback.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_worker_settings_t.cs
+${CMAKE_BINARY_DIR}/intermediate/cuvis_csil/cuvis_session_item_type_t.cs
 )
 
 
@@ -75,8 +75,8 @@ target_compile_definitions(${target_name} PRIVATE -D CUBERT_cbrt_path="${CBRT_PA
 
 set_target_properties(${target_name} PROPERTIES COMMON_LANGUAGE_RUNTIME "")
 
-file(MAKE_DIRECTORY ${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csharp/lib)
-add_custom_command(TARGET ${target_name} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${target_name}> ${EXECUTABLE_OUTPUT_PATH}/intermediate/cuvis_csharp/lib/ )
+file(MAKE_DIRECTORY ${CMAKE_BINARY_DIR}/intermediate/cuvis_csharp/lib)
+add_custom_command(TARGET ${target_name} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${target_name}> ${CMAKE_BINARY_DIR}/intermediate/cuvis_csharp/lib/ )
 
 
 target_compile_options(${target_name}
